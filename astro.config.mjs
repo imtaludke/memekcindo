2cf11af54c9593aa49160cdf8b80d1f0a6d60e08
imtaludke <imtaludke@gmail.com>
2025-07-04 00:30:15 +0700

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,12 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://memekcindo.pages.dev',
   output: 'server',
   adapter: cloudflare(),
   integrations: [
-    sitemap({
-      changefreq: 'daily',
-      priority: 1,
-      filename: 'sitemap.xml',
-    }),
   ],
   vite: {
     ssr: {
